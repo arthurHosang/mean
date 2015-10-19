ed2563ee23d1aef5b3f9ac6d4a5f756064290464
arthurHosang <arthur.rhc@gmail.com>
2015-10-18 23:15:43 -0200

    Implementado a inserção na Lista de Contatos Script no final da 4ª video aula

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,11 +2,24 @@
  * Created by arthur on 18/10/15.
  */
 var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log("Hello World from controller");
 
-    $http.get('/contactlist').success(function(response) {
-        console.log("Ive got the data I requested");
-        $scope.contactlist = response;
-    })
+    var refresh = function() {
+        $http.get('/contactlist').success(function (response) {
+            console.log("Ive got the data I requested");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    };
+
+    refresh();
+
+    $scope.addContact = function () {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function (response) {
+            console.log(response);
+            refresh();
+        })
+    };
 }]);
